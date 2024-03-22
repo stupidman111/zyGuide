e78e0ec294566d385638a692fc4df1ff51b40bef
zy <2456306700@qq.com>
2024-03-23 01:14:19 +0800

    vault backup: 2024-03-23 01:14:18

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🐴 Quartz 4.0",
+    pageTitle: "🐴 Zy's 知识学爆",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
