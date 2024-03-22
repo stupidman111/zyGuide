07de67a8429892fa21d9ccef64066182a52dd99b
zy <2456306700@qq.com>
2024-03-22 23:32:32 +0800

    vault backup: 2024-03-22 23:32:32
    Affected files:
    content/.obsidian/app.json
    content/.obsidian/appearance.json
    content/.obsidian/community-plugins.json
    content/.obsidian/core-plugins-migration.json
    content/.obsidian/core-plugins.json
    content/.obsidian/plugins/obsidian-git/data.json
    content/.obsidian/plugins/obsidian-git/main.js
    content/.obsidian/plugins/obsidian-git/manifest.json
    content/.obsidian/plugins/obsidian-git/styles.css
    content/.obsidian/themes/Obsidian Nord/manifest.json
    content/.obsidian/themes/Obsidian Nord/theme.css
    content/.obsidian/themes/Obsidianite/manifest.json
    content/.obsidian/themes/Obsidianite/theme.css
    content/.obsidian/workspace.json
    quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🐴 Quartz 4.0",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
